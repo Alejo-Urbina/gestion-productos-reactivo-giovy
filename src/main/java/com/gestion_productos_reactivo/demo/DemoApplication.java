0284d3952cb7090e303b1f833b489755b5ca600d
Giovy <alejoub1998@gmail.com>
2025-04-23 21:30:33 -0500

    Se inicia proyecto de parcial 1B

---
+++ src/main/java/com/gestion_productos_reactivo/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_productos_reactivo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
